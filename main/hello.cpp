88fb60da856f4a8c1f024e9b17a17621ee25a370
sladereaperr <karanjadhav2003@gmail.com>
2025-03-18 10:56:03 +0530

    First Update

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "This is Karan and I have made this change\n";
   return 0;
 }
